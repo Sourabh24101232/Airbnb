985eb435bf4fafd16b21db089506384e216ffc61
Sourabh24101232 <sourabhkumar920652@gmail.com>
2025-12-22 21:57:02 +0530

    Create schema.js

---
+++ schema.js
@@ -0,0 +1,25 @@
+const Joi = require("joi");
+
+// Listing validation
+const listingSchema = Joi.object({
+  listing: Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    price: Joi.number().min(0).required(),
+    image: Joi.object({
+      url: Joi.string().uri().allow("", null)
+    }).required()
+  }).required()
+});
+
+// Review validation
+const reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().min(1).max(5).required(),
+    comment: Joi.string().required()
+  }).required()
+});
+
+module.exports = { listingSchema, reviewSchema };
